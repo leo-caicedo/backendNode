4a53b75b96fb0f2aa2224aaa7605fd803de994e0
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-25 21:50:55 -0500

    Respuestas coherentes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const response = require('./network/response');
 
 var app = express();
 app.use(bodyParser.json());
@@ -9,12 +10,16 @@ app.use(router);
 
 router.get('/message', (req, res) => {
     console.log(req.headers);
-    res.send('Hola desde GET');
+    //res.send('Hola desde GET');
+    response.success(req, res, 'Vamos con toda!!! Si pude con Django, Nodejs es pan comido :)');
 });
 router.post('/message', (req, res) => {
     console.log(req.query);
-    console.log(req.body);
-    res.status(201).send({ body: 'Creado satisfactoriamente' });
+    if (req.query.error == "ok") {
+        response.error(req, res, 'Error Simulado', 401); 
+    } else {
+        response.success(req, res, 'Todo va muy bien :)', 201); 
+    }
 });
 
 app.listen(3000);
