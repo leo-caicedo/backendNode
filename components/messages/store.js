5759430051cfd70da2dbd49edaa52cdbecf5b3cb
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-26 20:13:40 -0500

    MongoDB: Almacenar y leer datos

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,11 +1,27 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+const { config } = require('../../config');
+
+
+db.Promise = global.Promise
+db.connect(config.dbHost, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+  .then(() => console.log('[db] Conectada con éxito'))
+  .catch(err => console.error('[db]', err));
+
 
 const addMessage = (messages) => {
-    list.push(messages);
+    //list.push(messages);
+    const myMessage = new Model(messages);
+    myMessage.save();
 }
 
-const getMessages = () => {
-    return list;
+const getMessages = async () => {
+    //return list;
+    const messages = await Model.find()
+    return messages;
 }
 
 module.exports = {
