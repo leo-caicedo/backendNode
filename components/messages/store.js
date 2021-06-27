655e05dbde0e7cbc6faf0cecb31a1cca6ffc4b70
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-27 13:03:39 -0500

    Relacionando nuestras entidades

--- components/messages/store.js
+++ components/messages/store.js
@@ -7,12 +7,21 @@ const addMessage = (messages) => {
 }
 
 const getMessages = async (filterUser) => {
-    let filter = {};
-    if (filterUser !== null) {
-        filter = { user: filterUser }
-    }
-    const messages = await Model.find(filterUser)
-    return messages;
+    return new Promise((resolve, reject) => {
+        let filter = {};
+        if (filterUser !== null) {
+            filter = { user: filterUser }
+        }
+        Model.find(filter)
+            .populate('user')
+            .exec((error, populated) => {
+                if (error) {
+                    reject(error);
+                    return false;
+                }
+                resolve(populated);
+            });
+    });
 }
 
 const updateText = async (id, messages) => {
