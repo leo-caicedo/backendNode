5219bf5eb36f39d6c46f12d504a1a04fd9ba5bc0
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-27 11:51:33 -0500

    Escalando la arquitectura: Múltiples entidades

---
+++ components/users/controller.js
@@ -0,0 +1,15 @@
+const store = require('./store');
+
+const adduser = (name) => {
+    if (!name) {
+        return Promise.reject('Invalid name');
+    }
+    const user = {
+        name,
+    };
+    return store.add(user);
+}
+
+module.exports = {
+    addUser,
+}
--- network/routes.js
+++ network/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const messages = require('../components/messages/network');
+const messages = require('../components/users/network');
 
 const routes = (server) => {
     server.use('/messages', messages);
+    server.use('/users', users);
 }
 
 module.exports = routes;
